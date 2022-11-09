9754aa885a6f0cc8119815b95fd6567f2bfd7d6f
15.6.4 <117864191+GModule@users.noreply.github.com>
2022-11-09 17:27:04 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,7 +2,7 @@
 // By Amy
 (function() {
     var x = document.createElement("script");
-    x.src = "URL//";
+    x.src = "https://raw.githubusercontent.com/GModule/Tricksplit/main/silver.js";
     x.onload = alert("Loaded GModule!");
     document.head.appendChild(x);
 })()
